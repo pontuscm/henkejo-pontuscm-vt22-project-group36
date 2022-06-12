f2de211a1d0ddb5eac20c6ca0ff96006b9ea64ef
Pontus Cowling Mantefors <pontuscm@kth.se>
2022-06-12 15:30:47 +0200

    ADD: detailsView shows title, image, example ingredient and description

--- src/views/detailsView.js
+++ src/views/detailsView.js
@@ -3,11 +3,7 @@ function DetailsView(props) {
     function renderImageAndPriceCB(props) {
         return <div class="detailsViewImageAndPrice">
             <span>
-                <img class="detailsViewImage" src={props.dishData.image} height="100"></img>
-            </span>
-            <span class="detailsViewPrice">
-                {"Price: " + props.dishData.pricePerServing.toFixed(2) + " per serving. "}
-                {"For " + props.guests + " guests: " + (props.dishData.pricePerServing*props.guests).toFixed(2) + " total."}
+                <img class="detailsViewImage" src={props.dishData.drinks[0].strDrinkThumb} height="100"></img>
             </span>
         </div>
     }
@@ -33,15 +29,16 @@ function DetailsView(props) {
         <div class="detailsView">
             <button class="addToMenuButton" disabled={props.isDishInMenu} onClick={addToMenuACB}>Add to menu!</button>
             <button onClick={onCancelButtonClickACB}>Cancel</button>
-            <div class="detailsViewTitle">{props.dishData.title}</div>
+            <div class="detailsViewTitle">{props.dishData.drinks[0].strDrink}</div>
             {renderImageAndPriceCB(props)}
             <div class="detailsViewIngredients">
-                <table>
-                    {props.dishData.extendedIngredients.map(renderIngredientsCB)}
-                </table>
+                <div>Place for ingredients list, only first added as example:</div>
+                <tr>
+                    <td class="detailsViewIngredientName">{props.dishData.drinks[0].strIngredient1}</td>
+                    <td class="detailsViewIngredientAmount">{props.dishData.drinks[0].strMeasure1}</td>
+                </tr>
             </div>
-            <div class="detailsViewInstructions">{props.dishData.instructions}</div>
-            <a class="detailsViewLink" target="_blank" href={props.dishData.sourceUrl}>More information</a>
+            <div class="detailsViewInstructions">{props.dishData.drinks[0].strInstructions}</div>
         </div>
     )
 }
