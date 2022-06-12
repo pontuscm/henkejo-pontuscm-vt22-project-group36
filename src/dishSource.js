29ac6b34283e0f5addd1884043a7995005b1ae95
Pontus Cowling Mantefors <pontuscm@kth.se>
2022-06-12 14:48:07 +0200

    ADD: Request to cocktailDB works and results are returned

--- src/dishSource.js
+++ src/dishSource.js
@@ -1,7 +1,6 @@
 import { BASE_URL,API_KEY } from "./apiConfig";
 
 function treatHTTPResponseACB(response){
-    /*TODO throw if the HTTP response is not 200, otherwise return response.json()*/
     if(response.status !== 200) {
         throw new Error('HTTP != 200');
     } else {
@@ -9,35 +8,32 @@ function treatHTTPResponseACB(response){
     }
 }
 
-function getDishDetails(params) {
-    
-    return fetch(BASE_URL+'recipes/'+params+'/information', {  // object literal
-        "method": "GET",              // HTTP method
-        "headers": {                  // HTTP headers, also object literal
-     'X-Mashape-Key': API_KEY,
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-     } // end of headers object
-    }/* end of second fetch parameter, object */
-    ).then(treatHTTPResponseACB);
+function transformResultACB(response) {
+    //console.log(response);
+    return response;
 }
 
-function transformResultACB(response) {
-    return response.results;
+function getDishDetails(params) {
+    console.log(BASE_URL+"lookup.php?i="+params);
+    return fetch(
+        BASE_URL+"lookup.php?i="+params, 
+        {
+            "method": "GET",
+            "headers": {}
+        }
+    ).then(treatHTTPResponseACB);
 }
 
 function searchDishes(params) {
     let searchParams = new URLSearchParams(params);
-    searchParams.toString();
-    return fetch(BASE_URL+'recipes/search?'+searchParams.toString(), {  // object literal
-        "method": "GET",              // HTTP method
-        "headers": {                  // HTTP headers, also object literal
-     'X-Mashape-Key': API_KEY,
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-     } // end of headers object
-    }/* end of second fetch parameter, object */
+    console.log(BASE_URL+"search.php?s="+searchParams.get("query").toString());
+    return fetch(
+        BASE_URL+"search.php?s="+searchParams.get("query").toString(), 
+        {
+            "method": "GET",
+            "headers": {},
+        }
     ).then(treatHTTPResponseACB).then(transformResultACB);
-
 }
-
 
 export {searchDishes,getDishDetails};
