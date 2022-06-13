4e05df1d491077e71b9d9dd21e8b29df0e97829d
Pontus Cowling Mantefors <pontuscm@kth.se>
2022-06-13 15:09:18 +0200

    ADD: Updates gitignore to include config files in the repo

---
+++ src/apiConfig.js
@@ -0,0 +1,8 @@
+const BASE_URL="https://www.thecocktaildb.com/api/json/v1/1/";
+const ALT_BASE_URL="https://the-cocktail-db.p.rapidapi.com"
+const API_KEY="1";
+
+const OLD_BASE_URL="https://brfenergi.se/iprog/group/10/";
+const OLD_API_KEY="3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+
+export {BASE_URL, API_KEY};
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyAe_jt1mBavQRmqAzJUN2aiNQyCqFiiGQ4",
+    authDomain: "dh2642-group36-project.firebaseapp.com",
+    databaseURL: "https://dh2642-group36-project-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "dh2642-group36-project",
+    storageBucket: "dh2642-group36-project.appspot.com",
+    messagingSenderId: "692546163652",
+    appId: "1:692546163652:web:151931fd95153200db8a91"  
+};
